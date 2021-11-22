4fc50cf10b33a0893251981079a30eae5cda548b
ghaner <51288907+ghaner@users.noreply.github.com>
2021-11-22 04:35:27 +0000

    Update Get_Target_Info.sh

--- script/com_script/Get_Target_Info.sh
+++ script/com_script/Get_Target_Info.sh
@@ -73,14 +73,7 @@
 		fi
 	;;
 	esac
-	[ ${REGULAR_UPDATE} == "true" ] && {
-		AutoUpdate_Version=$(egrep -o "V[0-9].+" ${Home}/package/base-files/files/bin/AutoUpdate.sh | awk 'END{print}')
-	} || AutoUpdate_Version=OFF
-	In_Firmware_Info="${Home}/package/base-files/files/bin/openwrt_info"
-	Github_Release="${Github}/releases/download/AutoUpdate"
-	Github_UP_RELEASE="${Github}/releases/AutoUpdate"
-	Openwrt_Version="${REPO_Name}-${TARGET_PROFILE}-${Compile_Date}"
-	Egrep_Firmware="${LUCI_Name}-${REPO_Name}-${TARGET_PROFILE}"
+	
 
 
 
