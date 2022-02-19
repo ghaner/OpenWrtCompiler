633e1608d598c1b9f8ffb5e25793b04f5a40484a
ghaner <51288907+ghaner@users.noreply.github.com>
2022-02-19 23:59:56 +0000

    Update Package_config.sh

--- script/diy_script/Package_config.sh
+++ script/diy_script/Package_config.sh
@@ -103,7 +103,7 @@ CONFIG_PACKAGE_wget-ssl=y
 CONFIG_PACKAGE_luci-app-filebrowser=y
 CONFIG_PACKAGE_luci-app-fileassistant=y
 
-#CONFIG_PACKAGE_luci-app-filetransfer=y
+
 CONFIG_PACKAGE_luci-app-ttyd=y
 
 ##services:
@@ -134,7 +134,7 @@ CONFIG_PACKAGE_luci-app-adbyby-plus=n
 CONFIG_PACKAGE_luci-app-arpbind=n
 CONFIG_PACKAGE_luci-app-autoreboot=n
 CONFIG_PACKAGE_luci-app-ddns=n
-
+CONFIG_PACKAGE_luci-app-filetransfer=n
 CONFIG_PACKAGE_luci-app-nlbwmon=n
 CONFIG_PACKAGE_luci-app-samba4=n
 #CONFIG_DEFAULT_luci-app-ssr-plus=n
