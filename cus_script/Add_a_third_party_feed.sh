b2677b9ad9e8d6527feb9c0127bf062f203c3e0e
ghaner <51288907+ghaner@users.noreply.github.com>
2023-04-29 21:49:51 +0800

    Update Add_a_third_party_feed.sh

--- cus_script/Add_a_third_party_feed.sh
+++ cus_script/Add_a_third_party_feed.sh
@@ -30,8 +30,8 @@ TIME() {
  
 echo
 TIME y "添加 a third-party feed:"
-TIME y "-添加 https://github.com/ghaner/OpenwrtPackage-Updater "
-git clone https://github.com/ghaner/OpenwrtPackage-Updater package/third-party-feed
+TIME y "-添加 https://github.com/ghaner/OpenWrt-Package "
+git clone https://github.com/ghaner/OpenWrt-Package package/third-party-feed
 
 
 chmod -R 755 package/third-party-feed
