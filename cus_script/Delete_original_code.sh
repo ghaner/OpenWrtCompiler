423c5bc689244e072fc121cbb852c7e678ef2ca3
ghaner <51288907+ghaner@users.noreply.github.com>
2024-03-21 07:40:47 +0800

    更新 Delete_original_code.sh
    -qbittorrent

--- cus_script/Delete_original_code.sh
+++ cus_script/Delete_original_code.sh
@@ -37,7 +37,9 @@ TIME r "cus_script/Delete_original_code.sh开始删除openwrt编译源代码中�
 TIME r "删除源码中与third-party feed重复的插件:"
 TIME r "删除argon主题"
 rm -rf ./package/lean/luci-theme-argon
-#rm -rf ./feeds/luci/applications/luci-app-aliyundrive-webdav
+
+TIME r "删除qbittorrent"
+rm -rf ./feeds/luci/applications/luci-app-qbittorrent_dynamic
 
 #rm -rf ./feeds/packages/net/smartdns
 #rm -rf ./feeds/packages/admin/netdata
