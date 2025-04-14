e1dbec6d02bfaf4e3134ab2cc4f1f30f778e60ce
ghaner <51288907+ghaner@users.noreply.github.com>
2025-04-14 11:25:10 +0800

    Update Device_config.sh

--- cus_script/Device_config.sh
+++ cus_script/Device_config.sh
@@ -28,10 +28,8 @@ TIME r "cus_script/Device_config.sh开始载入路由器型号:asus_rt-acrh17/ac
 cat >> .config <<EOF
 #MODULES:
 CONFIG_TARGET_ipq40xx=y
-#CONFIG_TARGET_ipq40xx_generic=y
-#CONFIG_TARGET_ipq40xx_generic_DEVICE_asus_rt-ac42u=y
-CONFIG_TARGET_ipq40xx_DEVICE_asus_rt-acrh17=y
-CONFIG_TARGET_BOARD="ipq40xx"
+CONFIG_TARGET_ipq40xx_generic=y
+CONFIG_TARGET_ipq40xx_generic_DEVICE_asus_rt-ac42u=y
 EOF
 
 
